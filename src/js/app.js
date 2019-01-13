cb5e9734b020a0be462a57b5fec08b0813277f39
Ryan Jennings <ryan@coda.life>
2019-01-13 12:31:44 -0800

    Update hostname js

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,20 @@
 
 function initDoodles() {
 
-    var width = ($(window).width()/4)*2;
-    var height = width * 0.68571428571;
+  var width = ($(window).width()/4)*2;
+  var height = width * 0.68571428571;
 
-    $("#doodle-gallery").simplecarousel({
-        next: $('.next'),
-        prev: $('.prev'),
-        slidespeed: 700,
-        height: height,
-        width: width,
-    });
+  $("#doodle-gallery").simplecarousel({
+    next: $('.next'),
+    prev: $('.prev'),
+    slidespeed: 700,
+    height: height,
+    width: width,
+  });
 }
 
 $(function() {
-  
+
   $(window).resize(function() {
     initDoodles();
   });
@@ -22,8 +22,13 @@ $(function() {
   initDoodles();
 
   if (window.location.hostname != 'coda.life' && window.location.hostname != 'www.coda.life') {
-    var parts = window.location.hostname.split('.')
-    document.title = parts.length > 3 ? parts[2] : parts.length > 2 ? parts[1] : parts[0]
+
+    if (window.location.hostname == 'ryanjennin.gs' || window.location.hostname == 'www.ryanjennin.gs') {
+      document.title = 'ryan jennings'
+    } else {
+      var parts = window.location.hostname.split('.')
+      document.title = parts.length > 3 ? parts[2] : parts.length > 2 ? parts[1] : parts[0]
+    }
     var items = $("#intro .brand, #brand .brand")
     items.css({ opacity: 0 })
     items.text(document.title)
