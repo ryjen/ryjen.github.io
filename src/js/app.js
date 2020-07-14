ada522fc858845472db079bb07c97fb7b1ff14c3
Ryan Jennings <ryan@micrantha.com>
2020-07-14 00:49:34 -0700

    Updated doodles

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,4 @@
 
-function redirect_business_card_url() {
-
-
-  if (Cookies.get('business_card_view')) {
-    return false
-  }
-
-  if (window.location.pathname !== '/') {
-    return false
-  }
-
-
-  if (window.location.hostname !== 'ryanjennin.gs' && window.location.hostname !== 'www.ryanjennin.gs') {
-    return false
-  }
-
-  Cookies.set('business_card_view', true, { expires: 7, path: '/' })
-
-  window.location = window.location.origin + "/resume"
-
-  return true
-}
-
 function initDoodles() {
 
   var width = ($(window).width()/4)*2;
@@ -38,42 +15,11 @@ function initDoodles() {
 
 $(function() {
 
-  //if (redirect_business_card_url()) {
-  //  return
-  //}
-
   $(window).resize(function() {
     initDoodles();
   });
 
   initDoodles();
-
-  var items = $("#intro .brand, #brand .brand")
-
-  var exclude = []
-
-  var overrides = { 
-    "ryanjennin.gs": "ryan jennings", 
-    "www.ryanjennin.gs": "ryan jennings", 
-    "localhost": "ryan jennings",
-    "ryjen.github.io": "ryan jennings",
-    "github.io": "ryan jennings"
-  }
-
-  var hostname = window.location.hostname
-
-  if (exclude.indexOf(hostname) == -1) {
-
-    if (overrides[hostname]) {
-      document.title = overrides[hostname]
-    } else {
-      var parts = hostname.split('.')
-      document.title = parts[0]
-    }
-    items.text(document.title)
-  }
-
-  items.fadeIn(900, function() { items.css({opacity: 1}) })
 
 })
 
